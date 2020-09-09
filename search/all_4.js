b9b99c31596d193cd641367b5090487fbe193820
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-09-09 18:39:55 +0000

    Deploy Erriez/ErriezDS1307 to github.com/Erriez/ErriezDS1307.git:gh-pages

--- search/all_4.js
+++ search/all_4.js
@@ -1,5 +1,6 @@
 var searchData=
 [
+  ['getdatetime',['getDateTime',['../class_erriez_d_s1307.html#a25dcc03f734e2189f710dde36e50fb01',1,'ErriezDS1307']]],
   ['getepoch',['getEpoch',['../class_erriez_d_s1307.html#a3534f588441ba96c6b12475f0d440f49',1,'ErriezDS1307']]],
   ['gettime',['getTime',['../class_erriez_d_s1307.html#aac10af9f0182da72b87e7634f5f166ef',1,'ErriezDS1307']]]
 ];
